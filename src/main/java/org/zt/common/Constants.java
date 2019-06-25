d0ac88744a3a47f77fd578e6319a8facb6b03223
gggg2221 <poasboy87@gmail.com>
2019-06-25 12:06:04 +0800

    0625

--- src/main/java/org/zt/common/Constants.java
+++ src/main/java/org/zt/common/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     public static final String ORDERIP = "10.10.203.19:8081";
     public static final String SIGNIP = "10.10.203.33:8185";
     public static final String OldSIGNIP = "10.10.203.33:8085";
-    public static final String ORDER_URL = "http://" + ORDERIP + "/Order-api-dispatcher/Order/dispatcher";
+    public static final String ORDER_URL = "http://" + ORDERIP + "/order-api-dispatcher/order/dispatcher";
     public static final String SIGN_URL = "http://" + SIGNIP + "/jscsp-signatory/serviceRequest/signatory";
     public static final String OldSIGN_URL = "http://" + OldSIGNIP + "/jscsp-signatory/signatory.servlet";
     //云订单redisIp
--- src/test/java/cloud/order/dk/JieShunDk.java
+++ src/test/java/cloud/order/dk/JieShunDk.java
@@ -18,13 +18,14 @@ public class JieShunDk extends AbstractTestNGSpringContextTests {
     private static String orderid=Constants.oderid;
 
     @Test(groups = "smoke", description = "捷顺代扣")
-    public static void jieshundk() throws SQLException {
+    public static void jieshundk() throws SQLException, InterruptedException {
         KafkaTools kf = new KafkaTools();
         //入场验签写kafka(验签case已经完成，此处不需要实现)
         //发送出场数据到kafka
         String outjson = "{\"isReal\":0,\"parkName\":\"\",\"ysMoney\":0.01,\"yhMoney\":0.0,\"inEquipName\":\"入口\",\"outMode\":\"NORMAL\",\"outOperator\":\"Admin\",\"outEquipCode\":\"2\",\"ssMoney\":0.01,\"freeMoney\":0,\"dkTag\":1,\"orderNo\":\"\",\"outCarPhoto\":\"20181213001/NISSP_IMG_PARK_OUT/20181112/1\",\"vehicleInfo\":\"{\\\"plateColor\\\":\\\"BLUE\\\"}\",\"overTimeFlag\":0,\"idno\":\"16100552\",\"inRecordId\":\"" + times + "\",\"payTypeName\":\"云代扣\",\"carNumber\":\"藏-JK1111\",\"inTime\":\"" + times + "\",\"itemId\":\"" + orderid + "\",\"outEquipName\":\"出口\",\"inEquipCode\":\"1\",\"parkCode\":\"20181213001\",\"hgMoney\":0.0,\"outTime\":\"" + times + "\"}";
         kf.produce(Constants.DkTopic, outjson);
         //查询dk订单是否成功
+        Thread.sleep(3000);
         String sql = "SELECT ORDER_NO from cs_biz_order where CREATE_TIME=\"" + times + "\" and status=0;";
         String order=MysqlJdbc.seledata("cloud-db", sql, "ORDER_NO");
         System.out.println(order);
