8aaaaabf8ca3923e8a6b76f91f998d067d84fae8
gggg2221 <poasboy87@gmail.com>
2020-08-21 14:43:47 +0800

    0821

--- src/main/java/org/zt/common/Constants.java
+++ src/main/java/org/zt/common/Constants.java
@@ -1,26 +1,35 @@
 package org.zt.common;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class Constants {
 
-
-    private Constants(){}
+//    private Constants(){}
 
     //获取项目路径
 //    public static final String property = System.getProperty("user.dir");
 
     // 请求服务url
     public static String HTTP="http://";
-    public static String ORDERIP = "10.10.203.19:8081";
-    public static String SIGNIP = "10.10.203.33:8185";
+    public static String HTTPS="https://";
+    public static String ORDERIP="10.10.203.19:8081";
+    public static String SIGNIP="10.10.203.33:8185";
     public static String OLDSIGNIP = "10.10.203.33:8085";
+    public static String SYXSIGNIP="syx.jslife.com.cn";
     public static String ORDER_URL = HTTP + ORDERIP + "/order-api-dispatcher/order/dispatcher";
     public static String SIGN_URL = HTTP + SIGNIP + "/jscsp-signatory/serviceRequest/signatory";
+    public static String SYXSIGN_URL = HTTP + SYXSIGNIP + "/jscsp-sign/serviceRequest/signatory";
     public static String OLDSIGN_URL = HTTP + OLDSIGNIP + "/jscsp-signatory/signatory.servlet";
     //云订单redisIp
     public static String REDISIP = "10.10.203.12";
     //车场密钥
-    public static String PARKSIG ="e0c8223b7ca64fa5bf4c0e2f97fd8811";
+    public static String PARKSIG ="a413a0614997409cba4e109320d41b5c";
+    //syx车场
+//    public static String PARKSIG ="ea4145b3d2960a4fb53bd1c11cb92977";
     //世梁车场密钥
 //    public static String PARKSIG ="b4a2b0ee3e3e274542baea7f0dcec399";
 
@@ -50,14 +59,16 @@ public class Constants {
     //捷顺车牌：藏-JK1111
     public static String JIESHUN="藏-JK2222";
 
-    //其他渠道车：深圳银联：藏-D12345 福州城市大脑：闽-A66666 招商车主平台
-    public static String othercarno="湘-C1FF87";
+    /**其他渠道车：深圳银联：藏-D12345 福州城市大脑：闽-A66666 招商车主平台 蒙-BJS000 东莞通：粤-SR1111 天府通：川-Q11111
+    小桔租车:藏-ZJSGGG1 GREEN 建行isv：藏-K78789
+     **/
+    public static String othercarno="藏-K78789";
 
-    //车场code 2018113019
+    //车场code 20181213001 2019120311
     public static String PARKCODE="20181213001";
 
     //微信验签反查json
-    public static String wxsigjson="{\"serviceId\":\"fc.park.signatoryResult.OrderQuery\",\"data\":{\"parkCode\":\"20181213001\",\"dataItems\":[{\"carNo\":\""+ WXISV +"\",\"inTime\":\""+ SATA +"\",\"vehicleInfo\":\"{\\\"plateColor\\\":\\\"BLUE\\\"}\"}]}}";
+    public static String wxsigjson="{\"serviceId\":\"fc.park.signatoryResult.OrderQuery\",\"data\":{\"parkCode\":\""+PARKCODE+"\",\"dataItems\":[{\"carNo\":\""+ WXISV +"\",\"inTime\":\""+ SATA +"\",\"vehicleInfo\":\"{\\\"plateColor\\\":\\\"BLUE\\\"}\"}]}}";
 
     //支付宝验签反查
     public static String zfbsigjson="{\"serviceId\":\"fc.park.signatoryResult.OrderQuery\",\"data\":{\"parkCode\":\"20181213001\",\"dataItems\":[{\"carNo\":\""+ ZFBISV +"\",\"inTime\":\""+ SATA +"\",\"vehicleInfo\":\"{\\\"plateColor\\\":\\\"BLUE\\\"}\"}]}}";
@@ -74,5 +85,6 @@ public class Constants {
 
     //其他渠道代扣出场数据
     public static String otheroutjson= "{\"isReal\":0,\"parkName\":\"梅test\",\"ysMoney\":0.01,\"yhMoney\":0.0,\"inEquipName\":\"车场入口\",\"outMode\":\"NORMAL\",\"outOperator\":\"Admin\",\"outEquipCode\":\"2\",\"ssMoney\":0.01,\"freeMoney\":0,\"dkTag\":1,\"orderNo\":\"\",\"outCarPhoto\":\"20181213001/NISSP_IMG_PARK_OUT/20181112/1\",\"vehicleInfo\":\"{\\\"plateColor\\\":\\\"BLUE\\\"}\",\"overTimeFlag\":0,\"idno\":\"01181016100552\",\"inRecordId\":\"" + ODERID + "\",\"payTypeName\":\"线上代扣\",\"carNumber\":\""+othercarno+"\",\"inTime\":\"" + SATA + "\",\"itemId\":\"" + ODERID + "\",\"outEquipName\":\"车场出口\",\"inEquipCode\":\"6\",\"parkCode\":\""+PARKCODE+"\",\"hgMoney\":0.0,\"outTime\":\"" + SATA + "\"}";
+//    public static String otheroutjson= "{\"isReal\":0,\"parkName\":\"梅test\",\"ysMoney\":0.01,\"yhMoney\":0.0,\"inEquipName\":\"车场入口\",\"outMode\":\"NORMAL\",\"outOperator\":\"Admin\",\"outEquipCode\":\"2\",\"ssMoney\":0.01,\"freeMoney\":0,\"dkTag\":1,\"orderNo\":\"\",\"outCarPhoto\":\"20181213001/NISSP_IMG_PARK_OUT/20181112/1\",\"vehicleInfo\":\"{\\\"plateColor\\\":\\\"BLUE\\\"}\",\"overTimeFlag\":0,\"idno\":\"01181016100552\",\"inRecordId\":\"" + ODERID + "\",\"payTypeName\":\"线上代扣\",\"carNumber\":\""+othercarno+"\",\"inTime\":\"2020-07-14 15:09:20\",\"itemId\":\"" + ODERID + "\",\"outEquipName\":\"车场出口\",\"inEquipCode\":\"6\",\"parkCode\":\""+PARKCODE+"\",\"hgMoney\":0.0,\"outTime\":\"" + SATA + "\"}";
 }
 
